202e75785b4c69f243f098e2a34c7173aace9383
pczyzewski <p.czyzewski5@gmail.com>
2023-12-14 20:52:45 +0100

    removed not needed fields in ExerciseForm.php

--- src/App/Form/ExerciseForm.php
+++ src/App/Form/ExerciseForm.php
@@ -9,33 +9,26 @@ use Gym\Domain\Enum\MuscleTagEnum;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ExerciseForm extends AbstractType
 {
     public const SERIES_TARGET_FIELD = 'series_target';
     public const REPETITION_TARGET_FIELD = 'repetition_target';
-    public const KILOGRAM_TARGET_FIELD = 'kilogram_target';
-    public const TAGS_FIELD = 'tags';
+    public const TAG_FIELD = 'tag';
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add(
-            self::TAGS_FIELD,
+            self::TAG_FIELD,
             ChoiceType::class,
             [
                 'label' => 'Mięsień',
                 'choices' => \array_combine(MuscleTagEnum::toArray(), MuscleTagEnum::toArray()),
-                'required' => false,
-                'attr' => [
-                    'data-type' => 'tags',
-                    'data-free-input' => 'false',
-                    'data-selectable' => 'false'
-                ],
+                'required' => true,
             ]
         );
-        $builder->get(self::TAGS_FIELD)->addModelTransformer(
+        $builder->get(self::TAG_FIELD)->addModelTransformer(
             new TagModelTransformer()
         );
 
@@ -63,27 +56,6 @@ class ExerciseForm extends AbstractType
                     'class' => 'input'
                 ],
             ],
-        );
-
-        $builder->add(
-            self::KILOGRAM_TARGET_FIELD,
-            IntegerType::class,
-            [
-                'label' => 'Ciężar',
-                'required' => true,
-                'attr' => [
-                    'value' => '10',
-                    'class' => 'input'
-                ]
-            ],
-        );
-
-        $builder->add(
-            'zapisz',
-            SubmitType::class,
-            [
-                'attr' => ['class' => 'button is-primary is-fullwidth']
-            ]
         );
     }
 }
