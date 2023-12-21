78bd174bed18d36b35d6bbed38595d39d2943e26
pczyzewski <p.czyzewski5@gmail.com>
2023-12-21 20:03:32 +0100

    added runner in exercise in progress view

--- public/js/exercise_in_progress_helper.js
+++ public/js/exercise_in_progress_helper.js
@@ -1,15 +1,21 @@
-function changeValue($targetClass, $action) {
+function changeValue($targetClass, $action, $step = null) {
     let $counter = $('.' + $targetClass);
     let $counterValue = parseInt($counter.html());
+    let $stepSec = null === $step ? 1 : $step;
 
     if ($action === 'increase') {
-        $counter.html($counterValue + 1);
+        $counter.html($counterValue + $stepSec);
     } else if ($counterValue > 1) {
-        $counter.html($counterValue - 1);
+        $counter.html($counterValue - $stepSec);
     }
 }
 
 function saveExerciseProgress($trainingId, $stationId, $exerciseId) {
+    var $counterJingle = document.getElementById('jingle');
+    var $counterValue = parseInt($.trim($('.seconds-counter').html()));
+
+    console.log($counterValue);
+
     let $data = {
         'training_id': $trainingId,
         'station_id': $stationId,
@@ -28,10 +34,19 @@ function saveExerciseProgress($trainingId, $stationId, $exerciseId) {
     $request.done(function($liftedWeightId) {
         let $lastRow = $('.table tr:last-child');
         if ($lastRow.length) {
-           $lastRow.after('<tr id="' + $liftedWeightId + '"><td>#'+ ($('.table tr').length + 1) + '</td><td>' + $data['repetition_count'] + ' x</td><td>' + $data['kilogram_count'] + ' kg</td><td><button onclick="deleteExerciseProgress(\'' + $liftedWeightId + '\')" class="button is-danger is-small is-pulled-right">usuń</button></td></tr>');
+            $lastRow.after('<tr id="' + $liftedWeightId + '"><td>#'+ ($('.table tr').length + 1) + '</td><td>' + $data['repetition_count'] + ' x</td><td>' + $data['kilogram_count'] + ' kg</td><td><button onclick="deleteExerciseProgress(\'' + $liftedWeightId + '\')" class="button is-danger is-small is-pulled-right">usuń</button></td></tr>');
         } else {
             $('.table tbody').append('<tr id="' + $liftedWeightId + '"><td>#1</td><td>' + $data['repetition_count'] + ' x</td><td>' + $data['kilogram_count'] + ' kg</td><td><button onclick="deleteExerciseProgress(\'' + $liftedWeightId + '\')" class="button is-danger is-small is-pulled-right">usuń</button></td></tr>');
         }
+
+        $('.seconds-counter').countdownTimer({
+            seconds: $counterValue,
+            loop: false,
+            callback: function () {
+                $('.seconds-counter').text($counterValue);
+                $counterJingle.play();
+            }
+        });
     });
 }
 
