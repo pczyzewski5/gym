7349fdd15e1199539720f08c50097b582b0ada1a
pczyzewski <p.czyzewski5@gmail.com>
2024-01-16 23:39:34 +0100

    fixed migrations

--- migrations/data/users.sql
+++
@@ -1,4 +0,0 @@
-TRUNCATE TABLE `users`;
-
-INSERT INTO `users` (`id`, `email`, `username`, `roles`, `password`, `is_active`, `created_at`) VALUES
-('c4b2555e-8a41-11ee-a5cd-0f69cc2384f2', 'nocny88@gmail.com', 'Druid Żab', '["ROLE_ADMIN"]', '$2y$13$7H0pJd1/lejjFPZsu4f84Oaof9/V84Y4cavj/jPDEVxoLk8fBa8Sy', 1, '2023-11-23 20:49:17');
